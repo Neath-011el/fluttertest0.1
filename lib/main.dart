726323523aa0fede36c51df6d446559ff8aade84
E2STEM <Dq,%msoy#RfZCqB52JGM>
2021-06-22 15:23:12 +0700

    Adding Reformating using Dartfmt

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Hello World from Brazil by Yuki')))));
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World from Brazil by Yuki'),
+        ),
+      ),
+    ),
+  );
 }
