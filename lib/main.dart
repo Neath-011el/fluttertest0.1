afa42bd6c698d40146aca28237fc6fc34e575e46
AnnaSa11 <savunvathana999@gmail.com>
2021-06-29 18:41:53 +0700

    Add my name

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
-          title:
-              Text('Hello World from Brazil by Yuki Sato, Prospere Kim, Yiyi, Vathana'),
+          title: Text(
+              'Hello World from Brazil by Yuki Sato, Prospere Kim, Yiyi, Vathana'),
         ),
       ),
     ),
