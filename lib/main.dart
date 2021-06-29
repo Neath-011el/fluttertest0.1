1b588ab303c21944ded03aca7d02760c09ca725b
E2STEM <Dq,%msoy#RfZCqB52JGM>
2021-06-29 15:15:16 +0700

    Adding re formmating

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World from Brazil by Yuki Sato, Prospere Kim, Yiyi, thida'),
+          title: Text('Hello World from Brazil, but living in  CAMBODIA  by Yuki Sato, Prospere Kim, Yiyi, thida'),
         ),
         body: Center(child: Text('Maneth'))
       ),
