c743ce50c6fee1c5b9a065a51e151584b7f3fb73
maneth <hoeunmaneth09@gmail.com>
2021-07-04 21:07:04 +0700

    branch testing

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar( backgroundColor: Colors.green
+        appBar: AppBar( backgroundColor: Colors.green,
           title: Text('Hello World from Brazil, but living in  CAMBODIA  by Yuki Sato, Prospere Kim, Yiyi, thida'),
         ),
-        body: Center(child: Text('Maneth hoeun'))
+        body: Center(child: Text('Maneth hoeun from the future'))
       ),
     ),
   );
